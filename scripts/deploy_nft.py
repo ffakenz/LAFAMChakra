323772c0681f8006464084c23313834d35b0a61d
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-01-23 16:19:27 +0100

    ADD img and metadata folder

--- scripts/deploy_nft.py
+++ scripts/deploy_nft.py
@@ -4,7 +4,7 @@ from brownie import network, accounts, config, LAFAMChakra
 def deploy():
     dev = accounts.add(config["wallets"]["from_key"])
     print("Deploying to " + network.show_active())
-    publish_source = True if os.getenv("ETHERSCAN_TOKEN") else False
+    publish_source = True if os.getenv("HARMONY_BLOCK_EXPLORER") else False
     base_uri = ""
     LAFAMChakra.deploy(base_uri, {"from": dev}, publish_source=publish_source)
     print("Deployed!")
