08102678f1972bf425716d21f1f87bdc1c5f3e33
Damian Albrun <damian.ea@gmail.com>
2022-01-25 14:32:08 +0100

    Fix baseURI

--- scripts/deploy_nft.py
+++ scripts/deploy_nft.py
@@ -5,7 +5,7 @@ def deploy():
     dev = accounts.add(config["wallets"]["from_key"])
     print("Deploying to " + network.show_active())
     publish_source = True if os.getenv("ETHERSCAN_TOKEN") else False
-    base_uri = "https://ipfs.io/ipfs/QmeRMJz2q8MgiZgAt2yDNjQu5wMspQfZTXjSp59LFBAUF3"
+    base_uri = "https://ipfs.io/ipfs/QmRwGfppo8ajddwRsdEAgm2q8tUFpPiBM5Hs8bMyifCGvK/"
     LAFAMChakra.deploy(base_uri, {"from": dev}, publish_source=publish_source)
     print("Deployed!")
 
