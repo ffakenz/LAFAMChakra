06d2fe356a0ffc7a43bdcd1c37f7073f50e39a77
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-01-25 22:42:17 +0100

    ADD prettier

--- contracts/LAFAMChakra.sol
+++ contracts/LAFAMChakra.sol
@@ -7,17 +7,32 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract LAFAMChakra is ERC1155, Ownable {
-    using SafeMath for uint;
-    using Strings for uint;
+    using SafeMath for uint256;
+    using Strings for uint256;
 
-    enum Chakra {RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET}
-    
-    event ChakrasAwarded(address avatar, uint[] chakras, uint[] amounts, uint time);
+    enum Chakra {
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        INDIGO,
+        VIOLET
+    }
+
+    event ChakrasAwarded(
+        address avatar,
+        uint256[] chakras,
+        uint256[] amounts,
+        uint256 time
+    );
 
     string _baseUri;
     string _uriExtension;
-    
-    constructor(string memory baseUri) ERC1155(string(abi.encodePacked(baseUri, "{id}", ".json"))) { 
+
+    constructor(string memory baseUri)
+        ERC1155(string(abi.encodePacked(baseUri, "{id}", ".json")))
+    {
         _baseUri = baseUri;
         _uriExtension = ".json";
     }
@@ -25,28 +40,37 @@ contract LAFAMChakra is ERC1155, Ownable {
     /**
      * @dev public abi
      */
-    function mintChakras(address avatar, uint chakraId, uint amount) 
-        public
-        onlyOwner {       
-
+    function mintChakras(
+        address avatar,
+        uint256 chakraId,
+        uint256 amount
+    ) public onlyOwner {
         require(
             isValidChakra(chakraId),
             string(abi.encodePacked("invalid chakra id:", chakraId.toString()))
         );
 
         _mint(avatar, chakraId, amount, "");
-        emit ChakrasAwarded(avatar, asSingletonArray(chakraId), asSingletonArray(amount), block.timestamp);
+        emit ChakrasAwarded(
+            avatar,
+            asSingletonArray(chakraId),
+            asSingletonArray(amount),
+            block.timestamp
+        );
     }
 
-    function mintChakrasBatch(address avatar, uint[] memory chakraIds, uint[] memory amounts)
-        public
-        onlyOwner {       
-        
-        for(uint i; i < chakraIds.length; i++) {
-            uint chakraId = chakraIds[i];
+    function mintChakrasBatch(
+        address avatar,
+        uint256[] memory chakraIds,
+        uint256[] memory amounts
+    ) public onlyOwner {
+        for (uint256 i; i < chakraIds.length; i++) {
+            uint256 chakraId = chakraIds[i];
             require(
                 isValidChakra(chakraId),
-                string(abi.encodePacked("invalid chakra id:", chakraId.toString()))
+                string(
+                    abi.encodePacked("invalid chakra id:", chakraId.toString())
+                )
             );
         }
 
@@ -54,23 +78,25 @@ contract LAFAMChakra is ERC1155, Ownable {
         emit ChakrasAwarded(avatar, chakraIds, amounts, block.timestamp);
     }
 
-    function airDropChakrasWithMint(address[] memory avatars, uint chakraId, uint amount)
-        public
-        onlyOwner { 
-
-        for(uint i; i < avatars.length; i++) {
+    function airDropChakrasWithMint(
+        address[] memory avatars,
+        uint256 chakraId,
+        uint256 amount
+    ) public onlyOwner {
+        for (uint256 i; i < avatars.length; i++) {
             address avatar = avatars[i];
             mintChakras(avatar, chakraId, amount);
         }
     }
 
-    function airDropChakrasWithTransfer(address[] memory avatars, uint chakraId, uint amount)
-        public
-        onlyOwner { 
-
-        uint count = avatars.length;
+    function airDropChakrasWithTransfer(
+        address[] memory avatars,
+        uint256 chakraId,
+        uint256 amount
+    ) public onlyOwner {
+        uint256 count = avatars.length;
         mintChakras(owner(), chakraId, count.mul(amount));
-        for(uint i; i < count; i++) {
+        for (uint256 i; i < count; i++) {
             address avatar = avatars[i];
             safeTransferFrom(owner(), avatar, chakraId, amount, "");
         }
@@ -79,42 +105,62 @@ contract LAFAMChakra is ERC1155, Ownable {
     /**
      * @dev public view functions
      */
-    function uri(uint256 id) override public view virtual returns (string memory) {
+    function uri(uint256 id)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
         return buildURI(id.toString());
     }
 
-    function buildURI(string memory id) internal view virtual returns (string memory) {
+    function buildURI(string memory id)
+        internal
+        view
+        virtual
+        returns (string memory)
+    {
         return string(abi.encodePacked(_baseUri, id, _uriExtension));
     }
 
-    function chackraBalanceOf(address avatar) public view returns (uint[] memory) {
-        uint chakraCount = maxChakra().add(1);
-        uint[] memory chakraBalance = new uint[](chakraCount);
-        for(uint i = minChakra(); i <= maxChakra(); i++) {
+    function chackraBalanceOf(address avatar)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256 chakraCount = maxChakra().add(1);
+        uint256[] memory chakraBalance = new uint256[](chakraCount);
+        for (uint256 i = minChakra(); i <= maxChakra(); i++) {
             chakraBalance[i] = balanceOf(avatar, i);
         }
         return chakraBalance;
     }
+
     /**
      * @dev pure functions
      */
-    function isValidChakra(uint id) public pure virtual returns (bool) {
+    function isValidChakra(uint256 id) public pure virtual returns (bool) {
         return id >= minChakra() && id <= maxChakra();
     }
 
-    function minChakra() public pure virtual returns (uint) {
-        return uint(type(Chakra).min);
+    function minChakra() public pure virtual returns (uint256) {
+        return uint256(type(Chakra).min);
     }
 
-    function maxChakra() public pure virtual returns (uint) {
-        return uint(type(Chakra).max);
+    function maxChakra() public pure virtual returns (uint256) {
+        return uint256(type(Chakra).max);
     }
 
     /**
      * @dev private pure functions
      */
-    function asSingletonArray(uint256 element) private pure returns (uint[] memory) {
-        uint[] memory array = new uint[](1);
+    function asSingletonArray(uint256 element)
+        private
+        pure
+        returns (uint256[] memory)
+    {
+        uint256[] memory array = new uint256[](1);
         array[0] = element;
         return array;
     }
--- contracts/LAFAMKeys.sol
+++ contracts/LAFAMKeys.sol
@@ -7,42 +7,42 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract LAFAMKeys is ERC1155, Ownable {
-    using SafeMath for uint;
-    using Strings for uint;
+    using SafeMath for uint256;
+    using Strings for uint256;
 
     struct NFTCollection {
-        uint id;
+        uint256 id;
         string name;
         string tag;
     }
-    mapping(uint => NFTCollection) public nftCollections;
-    
+    mapping(uint256 => NFTCollection) public nftCollections;
+
     mapping(string => bool) public tags;
     string[] public _tags;
-    mapping(string => uint[]) public _idsForTag;
+    mapping(string => uint256[]) public _idsForTag;
 
     event NFTAwarded(
         address indexed avatar,
-        string indexed tag, 
-        uint[] ids,
-        uint[] amounts,
-        uint indexed time
+        string indexed tag,
+        uint256[] ids,
+        uint256[] amounts,
+        uint256 indexed time
     );
-    
+
     string public _baseUri;
     string public _uriExtension;
     string private _name;
     string private _symbol;
 
-    constructor(
-        string memory baseUri
-    ) ERC1155(string(abi.encodePacked(baseUri, "{id}", ".json"))) { 
+    constructor(string memory baseUri)
+        ERC1155(string(abi.encodePacked(baseUri, "{id}", ".json")))
+    {
         _baseUri = baseUri;
         _uriExtension = ".json";
-        _name = "LAFAM";
+        _name = "LAFAMKeys";
         _symbol = "LAFAM";
     }
-    
+
     function initialize() public onlyOwner {
         addTAG("chakras");
         addNFTCollection(1, "ORANGE", "chakras");
@@ -57,42 +57,58 @@ contract LAFAMKeys is ERC1155, Ownable {
     // @dev onlyOwner
     function addTAG(string memory tag) public onlyOwner {
         validateTagNotExist(tag);
+        validateTagNotEmpty(tag);
 
         _tags.push(tag);
         tags[tag] = true;
     }
 
+    // depends on tag existance (addTAG)
     function addNFTCollection(
-        uint id,
-        string memory name, 
-        string memory tag 
+        uint256 id,
+        string memory name,
+        string memory tag
     ) public onlyOwner {
         validateIdNotExist(id);
         validateTagExists(tag);
         validateNameNotEmpty(name);
 
-        NFTCollection memory c = NFTCollection({
-            id: id,
-            name: name,
-            tag: tag
-        });
+        NFTCollection memory c = NFTCollection({id: id, name: name, tag: tag});
 
         nftCollections[id] = c;
         _idsForTag[tag].push(id);
     }
 
-     function mint(address avatar, string memory tag, uint id, uint amount) public onlyOwner {       
+    // depends on tag and token id existance (addNFTCollection)
+    function mint(
+        address avatar,
+        string memory tag,
+        uint256 id,
+        uint256 amount
+    ) public onlyOwner {
         validateTagExists(tag);
         validateIdExists(id);
         validateTagMatchForId(tag, id);
 
         _mint(avatar, id, amount, "");
-        emit NFTAwarded(avatar, tag, asSingletonArray(id), asSingletonArray(amount), block.timestamp);
+        emit NFTAwarded(
+            avatar,
+            tag,
+            asSingletonArray(id),
+            asSingletonArray(amount),
+            block.timestamp
+        );
     }
 
-    function mintBatch(address avatar, string memory tag, uint[] memory ids, uint[] memory amounts) public onlyOwner {       
+    // depends on tag and token id existance (addNFTCollection)
+    function mintBatch(
+        address avatar,
+        string memory tag,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) public onlyOwner {
         validateTagExists(tag);
-        for(uint id; id < ids.length; id++) {
+        for (uint256 id; id < ids.length; id++) {
             validateIdExists(id);
             validateTagMatchForId(tag, id);
         }
@@ -100,18 +116,30 @@ contract LAFAMKeys is ERC1155, Ownable {
         _mintBatch(avatar, ids, amounts, "");
         emit NFTAwarded(avatar, tag, ids, amounts, block.timestamp);
     }
-    
-    function airDropWithMint(address[] memory avatars, string memory tag, uint id, uint amount) public onlyOwner { 
-        for(uint i; i < avatars.length; i++) {
+
+    // depends on tag and token id existance (addNFTCollection) due to mint
+    function airDropWithMint(
+        address[] memory avatars,
+        string memory tag,
+        uint256 id,
+        uint256 amount
+    ) public onlyOwner {
+        for (uint256 i; i < avatars.length; i++) {
             address avatar = avatars[i];
             mint(avatar, tag, id, amount);
         }
     }
 
-    function airDropWithTransfer(address[] memory avatars, string memory tag, uint id, uint amount) public onlyOwner { 
-        uint count = avatars.length;
+    // depends on tag and token id existance (addNFTCollection) due to mint
+    function airDropWithTransfer(
+        address[] memory avatars,
+        string memory tag,
+        uint256 id,
+        uint256 amount
+    ) public onlyOwner {
+        uint256 count = avatars.length;
         mint(owner(), tag, id, count.mul(amount));
-        for(uint i; i < count; i++) {
+        for (uint256 i; i < count; i++) {
             address avatar = avatars[i];
             transferFrom(owner(), avatar, id, amount);
         }
@@ -120,20 +148,37 @@ contract LAFAMKeys is ERC1155, Ownable {
     /**
      * @dev public functions
      */
-    function uri(uint256 id) override public view virtual returns (string memory) {        
+
+    // depends on token id existance due to buildURI
+    function uri(uint256 id)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
         return buildURI(id);
     }
 
-    function idsForTag(string memory tag) public view returns (uint[] memory) {
+    function idsForTag(string memory tag)
+        public
+        view
+        returns (uint256[] memory)
+    {
         validateTagExists(tag);
 
         return _idsForTag[tag];
     }
 
-    function tagBalanceOf(address avatar, string memory tag) public view returns (uint[] memory) {
-        uint[] memory ids = idsForTag(tag);
-        uint[] memory tagBalance = new uint[](ids.length);
-        for(uint i; i < ids.length; i++) {
+    // depends on tag existance due to idsForTag
+    function tagBalanceOf(address avatar, string memory tag)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory ids = idsForTag(tag);
+        uint256[] memory tagBalance = new uint256[](ids.length);
+        for (uint256 i; i < ids.length; i++) {
             tagBalance[i] = balanceOf(avatar, i.add(1));
         }
         return tagBalance;
@@ -144,16 +189,13 @@ contract LAFAMKeys is ERC1155, Ownable {
         address to,
         uint256 id,
         uint256 amount
-    ) public {    
+    ) public {
         safeTransferFrom(from, to, id, amount, "");
     }
 
     // @dev validations
     function validateTagNotExist(string memory tag) public view {
-        require(
-            !tags[tag],
-            "invalid tag - already exists"
-        );
+        require(!tags[tag], "invalid tag - already exists");
     }
 
     function validateTagExists(string memory tag) public view {
@@ -163,69 +205,109 @@ contract LAFAMKeys is ERC1155, Ownable {
         );
     }
 
-    function validateIdNotExist(uint id) public view {
+    function validateIdNotExist(uint256 id) public view {
         require(
             nftCollections[id].id == 0,
             string(abi.encodePacked("invalid nft id - already exists", id))
         );
     }
 
-    function validateIdExists(uint id) public view {
+    function validateIdExists(uint256 id) public view {
         require(
             nftCollections[id].id != 0,
             string(abi.encodePacked("invalid id - does not exists:", id))
         );
     }
-    
+
     function validateNameNotEmpty(string memory name) public pure {
-        require(
-            bytes(name).length > 0,
-            "invalid name - empty"
-        );
+        require(bytes(name).length > 0, "invalid name - empty");
     }
 
-    function validateTagMatchForId(string memory tag, uint id) public view {
+    function validateTagNotEmpty(string memory tag) public pure {
+        require(bytes(tag).length > 0, "invalid tag - empty");
+    }
+
+    function validateTagMatchForId(string memory tag, uint256 id) public view {
         string memory current = nftCollections[id].tag;
         require(
             compareStrings(current, tag),
-            string(abi.encodePacked("invalid tag - does not match current:",
-                "id ", id, ", ",
-                "tag ", tag, ", ",
-                "current ", current
-            ))
+            string(
+                abi.encodePacked(
+                    "invalid tag - does not match current:",
+                    "id ",
+                    id,
+                    ", ",
+                    "tag ",
+                    tag,
+                    ", ",
+                    "current ",
+                    current
+                )
+            )
         );
     }
 
-    function validateNameMatchForId(string memory name, uint id) public view {
+    function validateNameMatchForId(string memory name, uint256 id)
+        public
+        view
+    {
         string memory current = nftCollections[id].name;
         require(
             compareStrings(current, name),
-            string(abi.encodePacked("invalid name - does not match current:",
-                "id ", id, ", ",
-                "name ", name, ", ",
-                "current ", current
-            ))
+            string(
+                abi.encodePacked(
+                    "invalid name - does not match current:",
+                    "id ",
+                    id,
+                    ", ",
+                    "name ",
+                    name,
+                    ", ",
+                    "current ",
+                    current
+                )
+            )
         );
     }
 
     /**
      * @dev private functions
      */
-    function buildURI(uint id) private view returns (string memory) {
+
+    // depends on tag and token id existance (addNFTCollection)
+    function buildURI(uint256 id) private view returns (string memory) {
         validateIdExists(id);
 
         NFTCollection memory nft = nftCollections[id];
         string memory fileDir = nft.tag;
         string memory fileName = nft.name;
-        return string(abi.encodePacked(_baseUri, fileDir, "/", fileName, _uriExtension));
+        return
+            string(
+                abi.encodePacked(
+                    _baseUri,
+                    fileDir,
+                    "/",
+                    fileName,
+                    _uriExtension
+                )
+            );
     }
 
-    function compareStrings(string memory a, string memory b) private pure returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    function compareStrings(string memory a, string memory b)
+        private
+        pure
+        returns (bool)
+    {
+        return (keccak256(abi.encodePacked((a))) ==
+            keccak256(abi.encodePacked((b))));
     }
 
-    function asSingletonArray(uint256 element) private pure returns (uint[] memory) {
-        uint[] memory array = new uint[](1);
+    function asSingletonArray(uint256 element)
+        private
+        pure
+        returns (uint256[] memory)
+    {
+        uint256[] memory array = new uint256[](1);
         array[0] = element;
         return array;
     }
