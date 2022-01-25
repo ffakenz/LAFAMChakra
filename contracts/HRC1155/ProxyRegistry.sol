346c4788917555081d958f7666607eaf90671450
Franco Testagrossa <franco.testagrossa@gmail.com>
2022-01-25 14:40:07 +0100

    REMOVE HRC1155 ~ PURE SHIT

--- contracts/HRC1155/ProxyRegistry.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.11;
-
-import "./OwnableDelegateProxy.sol";
-
-contract ProxyRegistry {
-	mapping(address => OwnableDelegateProxy) public proxies;
-}
